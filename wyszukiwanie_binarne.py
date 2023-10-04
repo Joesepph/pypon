9808c6aeeb9d492a88a468d083e0575b4993ff98
Joe <99735244+Joesepph@users.noreply.github.com>
2023-10-04 17:04:59 +0200

    Add files via upload

---
+++ wyszukiwanie_binarne.py
@@ -0,0 +1,30 @@
+def binary_search(szukana, tab):
+    l = 0
+    r = len(tab) - 1
+
+    while l <= r:
+        sr = (l + r) // 2
+
+        if tab[sr] == szukana:
+            return sr
+
+        if tab[sr] > szukana:
+            r = sr - 1
+        else:
+            l = sr + 1
+
+    return -1
+
+def main():
+    szukana = int(input("Wyszukaj liczbę: "))
+    tab = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+    result = binary_search(szukana, tab)
+
+    if result == -1:
+        print("Taka liczba nie istnieje")
+    else:
+        print(f"Liczba {szukana} występuje w indeksie {result}")
+
+if __name__ == "__main__":
+    main()
