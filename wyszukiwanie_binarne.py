ab2e3ea1e86f294975353d5f4d3ab62a6e993b0e
Joe <99735244+Joesepph@users.noreply.github.com>
2023-10-18 11:06:11 +0200

    Update wyszukiwanie_binarne.py

--- wyszukiwanie_binarne.py
+++ wyszukiwanie_binarne.py
@@ -1,4 +1,4 @@
-def binary_search(szukana, tab):
+def wyszukiwanie_binarne(szukana, tab):
     l = 0
     r = len(tab) - 1
 
@@ -19,12 +19,12 @@ def main():
     szukana = int(input("Wyszukaj liczbę: "))
     tab = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
-    result = binary_search(szukana, tab)
+    wynik = wyszukiwanie_binarne(szukana, tab)
 
-    if result == -1:
+    if wynik == -1:
         print("Taka liczba nie istnieje")
     else:
-        print(f"Liczba {szukana} występuje w indeksie {result}")
+        print(f"Liczba {szukana} występuje w indeksie {wynik}")
 
 if __name__ == "__main__":
     main()
